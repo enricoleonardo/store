4fba012228d26ee4c91622024c354be491cd5b16
Enrico Leonardo <enricoleonardo714@yahoo.co.id>
2020-09-29 23:05:07 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <WomanBanner />
+    <InstaBanner />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import WomanBanner from "@/components/WomanBanner.vue";
+import InstaBanner from "@/components/InstaBanner.vue";
+import Partner from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    WomanBanner,
+    InstaBanner,
+    Partner,
+    Footer,
+  },
+};
+</script>
